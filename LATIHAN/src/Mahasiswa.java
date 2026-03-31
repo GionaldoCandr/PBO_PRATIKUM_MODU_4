808f3d8f0728c79b15689603ef8795e491e6bbce
GionaldoCandr <gionaldocandrawansah@gmail.com>
2026-03-31 15:11:06 +0700

    welwew

---
+++ LATIHAN/src/Mahasiswa.java
@@ -0,0 +1,6 @@
+public class Mahasiswa {
+    String nama;
+    int umur;
+    String Jurusan;
+    
+}
--- PRETEST/src/App.java
+++ PRETEST/src/App.java
@@ -2,10 +2,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String nama = "Gio";
-        int umur= 69;
-        String jurusan= "Ilkom";
-        Mahasiswa mhsBaru = new Mahasiswa();
+        Mahasiswa mhsBaru = new Mahasiswa("Gionaldo", 20, "Ilmu Komputer");
         input.close();
     }
 }
