5497932005c1b9a26071a9ddb072c2214df744d9
GionaldoCandr <gionaldocandrawansah@gmail.com>
2026-03-31 14:43:42 +0700

    pretest

--- PRETEST/src/App.java
+++ PRETEST/src/App.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String nama = "Gio";
+        int umur= 69;
+        String jurusan= "Ilkom";
+        Mahasiswa mhsBaru = new Mahasiswa(nama, umur, jurusan);
+        mhsBaru.tampilkanProfil();
+        input.close();
     }
 }
---
+++ PRETEST/src/pretest.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+// 1. class
+class Mahasiswa {
+    String nama;
+    int umur;
+    String jurusanStudi;
+    
+}
+
+
